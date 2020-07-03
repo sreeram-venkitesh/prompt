09f8fd9d611d325024f602d04e85a8a20854b1cd
fillerInk <thatonephysicsnerd@gmail.com>
2020-07-03 11:39:46 +0530

    Retrieving data from frontend

--- prompt/index.js
+++ prompt/index.js
@@ -3,31 +3,41 @@ const express = require('express')
 const fetch = require('node-fetch')
 
 const app = express()
-// fetch('https://api.reddit.com/r/WritingPrompts/new.json')
-//     .then(res => console.log(res))
-//     .then(data => console.log(data))
-
-fetch('https://www.reddit.com/r/writingprompts/new.json')
-    .then(function(res) {
-        return res.json();   // Convert the data into JSON
-    })
-    .then(function(data) {
-        const index = Math.floor(Math.random() * 26) + 1
-        console.log(index)
-        flair = data.data.children[index].data['link_flair_richtext'][0]['t']   
-        console.log(flair)
-        if(flair == 'Writing Prompt' || flair == 'Reality Fiction' || flair == 'Simple Prompt'
-        || flair == 'Established Universe' || flair == 'Constrained Writing' || flair == 'Theme Thursday'){
-
-            console.log(data.data.children[index].data['title'].split(']')[1])
-        }
-    })
-    .catch(function(err) {
-        console.log(err);   // Log error if any
-    })
 
 
-app.get('/', (req, res) => res.sendfile('index.html'))
+app.use(express.static(__dirname + '/views'));
+
+
+app.set('view engine', 'ejs');
+
+
+// fetch('https://www.reddit.com/r/writingprompts/new.json')
+//     .then(function(res) {
+//         return res.json();   // Convert the data into JSON
+//     })
+//     .then(function(data) {
+//         const index = Math.floor(Math.random() * 26) + 1
+//         console.log(index)
+//         flair = data.data.children[index].data['link_flair_richtext'][0]['t']   
+//         console.log(flair)
+//         if(flair == 'Writing Prompt' || flair == 'Reality Fiction' || flair == 'Simple Prompt'
+//         || flair == 'Established Universe' || flair == 'Constrained Writing' || flair == 'Theme Thursday'){
+
+//             console.log(data.data.children[index].data['title'].split('] ')[1])
+//         }
+//     })
+//     .catch(function(err) {
+//         console.log(err);   // Log error if any
+//     })
+
+
+app.get('/', (req, res) => {
+  
+    
+    res.render('index')
+
+
+})
 
 
 const PORT = process.env.PORT || 3000
