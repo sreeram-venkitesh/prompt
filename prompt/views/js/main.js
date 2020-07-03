927dc4c350d84c4ab5f52ce86f2549aab0071e3e
fillerInk <thatonephysicsnerd@gmail.com>
2020-07-03 22:57:11 +0530

    Added button and typing effect in editor

--- prompt/views/js/main.js
+++ prompt/views/js/main.js
@@ -5,37 +5,77 @@ var i = 0;
 var txt = 'Prompts'; /* The text */
 var speed = 200; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
+function typeWriterTitle() {
   if (i < txt.length) {
     document.getElementById("title").innerText = document.getElementById("title").innerText.slice(0,-1)
     document.getElementById("title").innerText += txt.charAt(i);
     i++;
     document.getElementById("title").innerText += '|'
-    setTimeout(typeWriter, speed);
+    setTimeout(typeWriterTitle, speed);
   }
   else if(i == txt.length){
     document.getElementById("title").innerText = document.getElementById("title").innerText.slice(0,-1)
 
   }
 }
-typeWriter()
 
-fetch('https://www.reddit.com/r/writingprompts/new.json')
-    .then(function(res) {
-        return res.json();   // Convert the data into JSON
-    })
-    .then(function(data) {
-        const index = Math.floor(Math.random() * 26) + 1
-        console.log(index)
-        flair = data.data.children[index].data['link_flair_richtext'][0]['t']   
-        console.log(flair)
-        if(flair == 'Writing Prompt' || flair == 'Reality Fiction' || flair == 'Simple Prompt'
-        || flair == 'Established Universe' || flair == 'Constrained Writing' || flair == 'Theme Thursday'){
-            promptText.innerText = data.data.children[index].data['title'].split('] ')[1]
-        }
-    })
-    .catch(function(err) {
-        location.reload()
-        //promptText.innerText = 'There seems to be an error in fetching the data. Try reloading the page!'
-        console.log(err);   // Log error if any
-    })
+
+
+typeWriterTitle()
+
+const startButton = document.getElementById('startButton')
+const textArea = document.getElementById('textArea')
+
+startButton.addEventListener('click',()=>{
+
+
+  textArea.style.display = 'block'
+  textArea.scrollIntoView({ behavior: 'smooth', block: 'center' })
+  fetch('https://www.reddit.com/r/writingprompts/new.json')
+      .then(function(res) {
+          return res.json();   // Convert the data into JSON
+      })
+      .then(function(data) {
+          const index = Math.floor(Math.random() * 26) + 1
+          console.log(index)
+          flair = data.data.children[index].data['link_flair_richtext'][0]['t']   
+          console.log(flair)
+          if(flair == 'Writing Prompt' || flair == 'Reality Fiction' || flair == 'Simple Prompt'
+          || flair == 'Established Universe' || flair == 'Constrained Writing' || flair == 'Theme Thursday'){
+            promptText.value =""
+            story = data.data.children[index].data['title'].split('] ')[1]
+            console.log(story)
+            var j=0
+            function typeWriterText() {
+              if (j < story.length) {
+                promptText.value = promptText.value.slice(0,-1)
+                promptText.value += story.charAt(j);
+                j++;
+                promptText.value += '|'
+                setTimeout(typeWriterText, 30);
+              }
+              else if(j == story.length){
+                promptText.value = promptText.value.slice(0,-1)
+            
+              }
+            }  
+            typeWriterText()
+            //promptText.innerText = data.data.children[index].data['title'].split('] ')[1]
+            promptText.focus()
+          }else if(flair == 'Image Prompt' || flair == 'Media Prompt'){
+            location.reload()
+          }
+          if(data.data.children[index].data['title'].split('] ')[1] == 'undefined'){
+            location.reload()
+          }
+      })
+      .catch(function(err) {
+          location.reload()
+          //promptText.innerText = 'There seems to be an error in fetching the data. Try reloading the page!'
+          console.log(err);   // Log error if any
+      })
+
+})
+
+
+
